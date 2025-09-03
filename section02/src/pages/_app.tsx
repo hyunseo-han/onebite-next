a8d28518142a0e4ea69111791db5ac088316cc23
hyunseo <ols3040015@naver.com>
2025-09-03 13:08:12 +0900

    feat(app): 전역 헤더 내비게이션 및 useRouter 기반 페이지 전환 추가

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,13 +1,33 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push("/test");
+    // router.replace("/test");
+    // router.back();
+  };
   return (
     <>
-      <header>글로벌 헤더</header>
+      <header>
+        <Link href="/">index</Link>
+        &nbsp;
+        <Link href="/search">search</Link>
+        &nbsp;
+        <Link href="/book/1">book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
   );
 }
 // 모든 페이지 역할을 하는 컴포넌트의 부모 컴포넌트
 // 전체 페이지에 공통적으로 들어가는 레이아웃이나 상태 관리 등을 처리
+// a태그는 매번 서버에서 새로고침이 일어나서 속도가 느리다
+// 지금 클라이언트 사이드 렌더링 방식으로 페이지 전환 -> 굿
+// 함수 내부에서도 페이지를 클라이언트 사이드 렌더링 방식으로 이동 가능
